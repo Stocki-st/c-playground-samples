e7a4bfd3d39edca381b3463f5377974a6fb2389c
Stocki-st <141497133+Stocki-st@users.noreply.github.com>
2025-01-27 19:03:26 +0100

    Create find_min_max.c

---
+++ find_min_max.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void findMinMax(int arr[], int n, int *min, int *max) {
+    *min = arr[0];
+    *max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < *min) {
+            *min = arr[i];
+        }
+        if (arr[i] > *max) {
+            *max = arr[i];
+        }
+    }
+}
+
+int main() {
+    int arr[] = {12, 3, 45, 7, 23, 89, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int min, max;
+    findMinMax(arr, n, &min, &max);
+    printf("Minimum: %d, Maximum: %d\n", min, max);
+    return 0;
+}
